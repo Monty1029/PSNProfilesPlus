e984d2081341284bcdafc46537df7f7de74451fc
Monty1029 <dhanani.monty@gmail.com>
2017-06-29 22:31:37 -0400

    fixed bug with overriding !important on css

--- colorChange.js
+++ colorChange.js
@@ -2,9 +2,9 @@ var platinum = document.getElementsByClassName("platinum");
 var completed = document.getElementsByClassName("completed");
 
 for (var i = 1; i < platinum.length; i++) {
-    platinum[i].style.background = "#dde7fb";
+    platinum[i].setAttribute("style", "background-color: #dde7fb !important");
 }
 
 for (var i = 0; i < completed.length; i++) {
-    completed[i].style.background = "#e8ffe4";
+    platinum[i].setAttribute("style", "background-color: #e8ffe4 !important");
 }
