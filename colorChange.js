1d23524623ed847b5f5e1107064ebe3cb59f663d
Monty1029 <dhanani.monty@gmail.com>
2017-06-29 22:05:35 -0400

    fixed bug where total platinums gets highlighted

--- colorChange.js
+++ colorChange.js
@@ -1,7 +1,7 @@
 var platinum = document.getElementsByClassName("platinum");
 var completed = document.getElementsByClassName("completed");
 
-for (var i = 0; i < platinum.length; i++) {
+for (var i = 1; i < platinum.length; i++) {
     platinum[i].style.background = "#dde7fb";
 }
 
