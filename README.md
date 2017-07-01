5d3af4a457daa3d46c5b9ab05d30dbd08883a3a7
Monty1029 <dhanani.monty@gmail.com>
2017-07-01 15:01:13 -0400

    Red color PSN ID for hacker accounts

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ A Chrome Extension that improves a user's experience on <a href="https://psnprof
 <b>Current Features</b><br>
 Link to Amazon to purchase game.<br>
 Total trophy points for the base game where applicable.<br>
-Making the row colours of completed games on trophy list more visible for monitors with low colour accuracy.
+Making the row colours of completed games on trophy list more visible for monitors with low colour accuracy.<br>
+PSN ID color is red for hacker accounts.
 
 <b>In-progresss Features</b><br>
 
--- colorChange.js
+++ colorChange.js
@@ -1,10 +1,20 @@
 var platinum = document.getElementsByClassName("platinum");
 var completed = document.getElementsByClassName("completed");
 
+//Blue for platinum
 for (var i = 1; i < platinum.length; i++) {
     platinum[i].setAttribute("style", "background-color: #dde7fb !important");
 }
 
+//Green for 100%
 for (var i = 0; i < completed.length; i++) {
     completed[i].setAttribute("style", "background-color: #e8ffe4 !important");
+}
+
+//Red for hacker account
+var rank = document.getElementsByClassName("rank");
+if (rank.length == 0) {
+    console.log("yay");
+    var username = document.getElementsByClassName("username");
+    username[0].setAttribute("style", "color: #ff0000 !important");
 }
